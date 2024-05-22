3bfe5c40c1893cdbe3e79e4c1147f2aa01fc0feb
moazelgandy2 <moazhazem2005@icloud.com>
2024-05-23 00:20:12 +0300

    refactor: Update NavBar component to include maxCount prop and handle credit counter display

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,17 +1,18 @@
 import { UserButton } from "@clerk/nextjs";
 import { Menu } from "lucide-react";
-import { getLimitCount } from "@/lib/a-limit";
+import { getLimitCount, getMaxCount } from "@/lib/a-limit";
 
 import MobileSideBar from "@/components/mobile-side-bar";
 import { checkSubscription } from "@/lib/subscription";
 
 const NavBar = async () => {
+  const maxCount = await getMaxCount();
   const limit = await getLimitCount();
   const isPro = await checkSubscription();
   return (
     <>
       <div className="flex items-start p-4">
-        <MobileSideBar isPro={isPro} limit={limit} />
+        <MobileSideBar isPro={isPro} limit={limit} maxCount={maxCount} />
         <div className="flex w-full justify-end">
           <UserButton afterSignOutUrl="/" />
         </div>
